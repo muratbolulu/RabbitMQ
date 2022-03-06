9698ca9c73aa9bedcec85dc5a912ccc81db70773
Murat Bolulu <murat.bolulu@digilera.com>
2022-03-06 19:45:55 +0300

    Add project files.

---
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -0,0 +1,24 @@
+﻿
+//Console.WriteLine("Hello, World!");
+
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://bxjaidjr:hf7zKNpwJDGxBjcAwBiXX_6gYrTgLq4k@toad.rmq.cloudamqp.com/bxjaidjr");
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+channel.QueueDeclare("hello-queue", true, false, false);
+
+Enumerable.Range(0, 50).ToList().ForEach(x =>
+{
+    string message = $"Message {x}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(string.Empty, "hello-queue", null, messageBody);
+    Console.WriteLine($"Mesaj Gönderilmiştir : {message}");
+});
+
+Console.ReadLine();
---
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -0,0 +1,30 @@
+﻿
+//Console.WriteLine("Hello, World!");
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://bxjaidjr:hf7zKNpwJDGxBjcAwBiXX_6gYrTgLq4k@toad.rmq.cloudamqp.com/bxjaidjr");
+
+var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+//channel.QueueDeclare("hello-queue", true, false, false);
+
+channel.BasicQos(0,1,false);
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("hello-queue", false, consumer);
+
+consumer.Received += (sender, args) =>
+{
+    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+    Thread.Sleep(1000);
+
+    Console.WriteLine("Gelen Mesaj:" + message);
+
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+Console.ReadLine();
