1375757f73fda1fec3828ff77a5f5067d418819d
Murat Bolulu <murat.bolulu@digilera.com>
2022-03-07 22:52:51 +0300

    direct

--- UdemyRabbitMQ.subscriber/Program.cs
+++ UdemyRabbitMQ.subscriber/Program.cs
@@ -1,40 +1,38 @@
-﻿
-//Console.WriteLine("Hello, World!");
-
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
 var factory = new ConnectionFactory();
-factory.Uri = new Uri("amqps://bxjaidjr:hf7zKNpwJDGxBjcAwBiXX_6gYrTgLq4k@toad.rmq.cloudamqp.com/bxjaidjr");
+factory.Uri = new Uri(
+    "amqps://bxjaidjr:hf7zKNpwJDGxBjcAwBiXX_6gYrTgLq4k@toad.rmq.cloudamqp.com/bxjaidjr");
 
-var connection = factory.CreateConnection();
+using var connection = factory.CreateConnection();
 
 var channel = connection.CreateModel();
-//channel.QueueDeclare("hello-queue", true, false, false);
-//channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);
-var randomQueueName = channel.QueueDeclare().QueueName;
-//var randomQueueName = "log-database-save-queue";//channel.QueueDeclare().QueueName;
-//channel.QueueDeclare(randomQueueName, true, false, false);
 
-//bind ederek subscriber gidince kuyrukta gitmesi için bind edildi.
-channel.QueueBind(randomQueueName,"logs-fanout","",null);
+channel.BasicQos(0, 1, false);
 
-channel.BasicQos(0,1,false);
 var consumer = new EventingBasicConsumer(channel);
-//channel.BasicConsume("hello-queue", false, consumer);
-channel.BasicConsume(randomQueueName, false, consumer);
 
-Console.WriteLine("Loglar dinleniyor");
+var queueName = "direct-queue-Critical";
 
-consumer.Received += (sender, args) =>
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Loglar dinleniyor..");
+
+consumer.Received += (sender, e) =>
 {
-    var message = Encoding.UTF8.GetString(args.Body.ToArray());
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
     Thread.Sleep(1000);
 
     Console.WriteLine("Gelen Mesaj:" + message);
 
-    channel.BasicAck(args.DeliveryTag, false);
+    //File.AppendAllText("log-critical.txt", message + "\n");
+
+    channel.BasicAck(e.DeliveryTag, true);
+    //String consumerTag = channel.BasicConsume(queueName, false, consumer);
 };
+//kuyruktaki mesajlardan 10 varsa 10 işliyor fakat ekrana 9 yazdırıyor.
+String consumerTag = channel.BasicConsume(queueName, false, consumer);
 
 Console.ReadLine();
